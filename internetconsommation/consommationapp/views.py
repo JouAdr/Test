a353373b3ab406797e4c7ab7e60101dca3dc5c7c
Jou <joaryandrian@gmail.com>
2021-09-17 23:40:58 +0300

    apply migration, create home template

--- internetconsommation/consommationapp/views.py
+++ internetconsommation/consommationapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import InternetConsumption
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    consommations = InternetConsumption.objects.all()
+    return render(request, 'home.html', {'consommations':consommations})
