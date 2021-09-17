2bf04f7d870a0fb4a9702cc9c127c90a1e2865b4
Jou <joaryandrian@gmail.com>
2021-09-18 00:52:45 +0300

    somme internet for all user

--- internetconsommation/consommationapp/views.py
+++ internetconsommation/consommationapp/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import InternetConsumption
+from django.contrib.auth.models import User
 
 def home(request):
+    # users = User.objects.all()
     consommations = InternetConsumption.objects.all()
-    return render(request, 'home.html', {'consommations':consommations})
+    consommations_upload = list()
+    consommations_download = list()
+    consommation_total = list()
+
+    for consommation in consommations:
+        consommations_upload.append(consommation.upload)
+        consommations_download.append(consommation.download)
+    
+    for i in range(len(consommations_upload)):
+        consommation_total.append(consommations_upload[i] + consommations_download[i])
+
+    return render(request, 'home.html', {'consommations':consommations, 'consommation_total':consommation_total})
