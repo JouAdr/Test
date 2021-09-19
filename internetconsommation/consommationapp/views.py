8b0a2258ef4a2f55bfe1bbad2ee5d778cc514592
Jou <joaryandrian@gmail.com>
2021-09-19 14:27:09 +0300

    add template user detail

--- internetconsommation/consommationapp/views.py
+++ internetconsommation/consommationapp/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
-from .models import InternetConsumption
 from django.contrib.auth.models import User
+from django.http import Http404
+from .models import InternetConsumption
 
 def home(request):
-    # users = User.objects.all()
     consommations = InternetConsumption.objects.all()
-    consommations_upload = list()
-    consommations_download = list()
-    consommation_total = list()
+    return render(request, 'home.html', {'consommations':consommations})
 
-    for consommation in consommations:
-        consommations_upload.append(consommation.upload)
-        consommations_download.append(consommation.download)
-    
-    for i in range(len(consommations_upload)):
-        consommation_total.append(consommations_upload[i] + consommations_download[i])
 
-    return render(request, 'home.html', {'consommations':consommations, 'consommation_total':consommation_total})
+
+def user_details(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+        consommations = InternetConsumption.objects.all()
+        consommations_total = list()
+
+        for consommation in consommations:
+            consommations_total.append([consommation.user.username, (consommation.upload + consommation.download)])
+
+    except User.DoesNotExist:
+        raise Http404    
+    return render(request, 'user_details.html', {'consommations':consommations, 'user':user, 'consommations_total':consommations_total})
